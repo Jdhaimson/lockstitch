2def4e3f7a8a0682504c843e2892018bbff1f392
Josh haimson <jdhaimson@gmail.com>
2013-05-26 23:30:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lockstitch::Application.config.secret_token = 'ea43113584a2a369faa00816e028190927cc7411573c38178b08070a588a67da602d3b57b0fda9ddc20c43ee25fe7518dce99c8d8bd401b2cc2705509a5fb417'
